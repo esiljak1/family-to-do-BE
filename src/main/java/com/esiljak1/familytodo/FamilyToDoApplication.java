869ef6a5ff41dd770b4d5be8b311cda72fca4077
Emin Siljak <esiljak1@etf.unsa.ba>
2022-09-08 19:26:21 +0200

    Initial commit

---
+++ src/main/java/com/esiljak1/familytodo/FamilyToDoApplication.java
@@ -0,0 +1,13 @@
+package com.esiljak1.familytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esiljak1/familytodo/FamilyToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esiljak1.familytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
