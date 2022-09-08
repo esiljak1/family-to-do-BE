5145a94958c51ef3ea3ef3f97bdc2c1fcc9bca49
Emin Siljak <esiljak1@etf.unsa.ba>
2022-09-08 20:13:06 +0200

    Made User model class an entity

--- src/main/java/com/esiljak1/familytodo/user/User.java
+++ src/main/java/com/esiljak1/familytodo/user/User.java
@@ -1,6 +1,20 @@
 package com.esiljak1.familytodo.user;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
     private Long id;
     private String name;
     private String surname;
